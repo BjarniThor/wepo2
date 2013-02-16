65873c9db24efa1c73612c0c0fe9a63098d6133e
Bjarni Thor <bjarni.thor.palsson@gmail.com>
2013-02-16 14:35:42 +0000

    Allt eins

--- Examtool/views.py
+++ Examtool/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import *
 
 def index(request):
     post = Examtool.objects.get(pk=1)
-    model = { "post" : post }
+    quest = Question.objects.get(pk=1)
+    model = { "post" : post,
+              "quest" : quest }
     # print post.text
     return render_to_response("index.html", model)
