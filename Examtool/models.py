ba64ac0e5bf981dfa22736e895dcd320da513eb8
Brynjólfur Hermannsson <brynjolfur.hermannsson@gmail.com>
2013-02-16 11:59:01 +0000

    tables making more sense

--- Examtool/models.py
+++ Examtool/models.py
@@ -1,16 +1,28 @@
 from django.db import models
+#from wepo.db import *
 
 # Create your models here.
 class Examtool (models.Model):
+    #e_author = models.ForeignKey(auth_user)
     name = models.CharField(max_length=64)
-    question = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    e_course = models.CharField(max_length=200)
+    e_expire = models.DateTimeField('date published')
     def __unicode__(self):
         return self.question
 
+class Question(models.Model):
+    e_q = models.ForeignKey(Examtool)
+    q_text = models.CharField(max_length=200)
+    def __unicode__(self):
+        return self.q_text
+
+
 class Choice(models.Model):
-    examtool = models.ForeignKey(Examtool)
+    c_to_q = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
+    c_value = models.BooleanField()
     votes = models.IntegerField(default=0)
     def __unicode__(self):
         return self.choice_text
+
+
--- Examtool/views.py
+++ Examtool/views.py
@@ -3,7 +3,7 @@ from Examtool.models import *
 from django.shortcuts import *
 
 def index(request):
-    post = Examtool.objects.get(pk=2)
+    post = Examtool.objects.get(pk=1)
     model = { "post" : post }
     # print post.text
     return render_to_response("index.html", model)
